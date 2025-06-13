07f3cc01dcb560d3208963b1db0abb4d42bae5fc
julio-murillo <julio.murillo@gmail.com>
2025-06-12 22:02:53 -0600

    June 12 22:02 PM

--- Part_1/introdemo/src/App.jsx
+++ Part_1/introdemo/src/App.jsx
@@ -1,14 +1,22 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        Hello {props.name}, you are {props.age} years old
       </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + age} />
+      <Hello name={name} age={age}/>
     </div>
   )
 }
