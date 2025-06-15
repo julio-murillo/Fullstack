9e229eacf74808096011982889baf29c120d0bf1
julio-murillo <julio.murillo@gmail.com>
2025-06-15 13:07:44 -0600

    commit for excersice 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  //console.log(props)
+  console.log(props)
   return (
     <>
       <h1>
@@ -44,8 +44,9 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -59,12 +60,13 @@ const App = () => {
       exercises: 14
     }
   ]
+}
 
   return (
     <div>
-      <Header course = {course} />
-      <Content parts = {parts}/>
-      <Total parts = {parts}/>
+      <Header course = {course.name} />
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
