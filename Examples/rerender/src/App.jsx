7b668ddb26e2f5a092fa2eae664876dfa6b55cbc
julio-murillo <julio.murillo@gmail.com>
2025-06-18 16:19:54 -0600

    rerender ans state example

--- Examples/rerender/src/App.jsx
+++ Examples/rerender/src/App.jsx
@@ -1,21 +1,39 @@
 import { useState } from "react"
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log('rendiering with counter value', counter)
 
-  const increaseByOne = () => setCounter(counter + 1)
-
+  const increaseByOne = () => {
+    console.log('increasing, value before',counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
   const setToZero = () => setCounter(0)
 
   return (
     <>
-    <div>{counter}</div>
-    <button onClick={increaseByOne}>
-      plus
-    </button>
-    <button onClick={setToZero}>
-      zero
-    </button>
+      <Display counter={counter}/>
+      <Button 
+        onClick={increaseByOne}
+        text = 'plus'
+      />
+      <Button
+        onClick={setToZero}
+        text = 'zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text = 'minus'
+      />
     </>
   )
 }
