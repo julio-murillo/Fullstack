86db04fbab0a73daddc2429aa0b9b93e5ffe078f
julio-murillo <julio.murillo@gmail.com>
2025-06-22 07:41:59 -0600

    Excersise 1.9 Unicafe Step 4 - conditional display

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,45 +3,54 @@ import { use, useState } from 'react'
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}
 </button>
 
-const Display = ({text, value}) => <div>{text} {value}</div>
+const Stats = ({good, neutral, bad}) => {
+  const all = good+neutral+bad
+  const avg = (good+(bad*-1))/(good+neutral+bad)
+  const pos = (good/(good+neutral+bad)*100)
+  if (all) {
+    return(
+      <>
+        <h2>statistics</h2>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {isNaN(avg) ? 0 : avg}</div>
+        <div>positive {isNaN(pos) ? 0 : pos+'%'}</div>
+     </>
+    )
+  } else {
+    return (
+      <>
+        <h2>statistics</h2>
+        <div>No feedback given</div>
+      </>
+    )
+  }
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [avg, setAvg] = useState(0)
-  const [positive, setPositive] = useState(0)
 
   const increaseGood = () => {
     console.log('increasing, value before',good)
     const updatedGood = good + 1
     setGood(updatedGood)
-    const updatedAll = updatedGood + neutral + bad
-    setAll(updatedAll)
-    setAvg((updatedGood + (bad * -1)) / updatedAll)
-    setPositive((updatedGood/updatedAll*100)+'%')
   }
 
   const increaseNeutral = () => {
     console.log('increasing, value before',neutral)
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
-    const updatedAll = good + updatedNeutral + bad
-    setAll(updatedAll)
-    setAvg((good + (bad * -1)) / updatedAll)
-    setPositive((good/updatedAll*100)+'%')
   }
 
   const increaseBad = () => {
     console.log('increasing, value before',bad)
     const updatedBad = bad + 1
     setBad(updatedBad)
-    const updatedAll = good + neutral + updatedBad
-    setAll(updatedAll)
-    setAvg((good + (updatedBad * -1)) / updatedAll)
-    setPositive((good/updatedAll*100)+'%')
   }
 
   return (
@@ -59,30 +68,10 @@ const App = () => {
         onClick={increaseBad}
         text = 'bad'
       />
-      <h2>statistics</h2>
-      <Display
-        text='good'
-        value={good}
-      />
-       <Display
-        text='neutral'
-        value={neutral}
-      />
-       <Display
-        text='bad'
-        value={bad}
-      />
-      <Display
-        text = 'all'
-        value = {all}
-      />
-      <Display
-      text = 'average'
-      value = {avg}
-      />
-      <Display
-      text='positive'
-      value = {positive}
+      <Stats
+        good={good}
+        neutral={neutral}
+        bad={bad}
       />
     </div>
   )
