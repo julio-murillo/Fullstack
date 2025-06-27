b74138b435994bc2093632811321d6ee53dc5007
null <null>
2025-06-27 17:43:15 -0600

    Excersise 2.1 Course information step 6

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,42 @@
+//const Header = (props) => <h1>{props.course}</h1>
+
+const Course= ({course}) => (
+  <>
+    <h1>{course.name}</h1>
+      {course.parts.map(part =>
+    <p key={part.id}>
+      {part.name} {part.exercises}
+    </p>
+    )}
+  </>
+)
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  const result = course.parts.map(part => part.name)
+  console.log(result)
+
+  return <Course course={course} />
+}
+
+export default App
