7e92e6a9123a1e9e40e8d1ab234074594c4dc582
julio-murillo <julio.murillo@gmail.com>
2025-06-29 12:22:05 -0600

    Excercise 2.9 - The Phonebook Setp 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('New name...')
   const [newNumber, setNewNumber] = useState('00-00-0000000')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -16,7 +19,8 @@ const App = () => {
     } else {
       const personObject = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id : persons.length + 1
       }
       setPersons(persons.concat(personObject))
       setNewName ('')
@@ -28,12 +32,12 @@ const App = () => {
   (
     <>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {persons.map(person => <div key={person.id}>{person.name} {person.number}</div>)}
     </>
   )
 
   const handleNameChange = (event) => {
-    //console.log(event.target.value)
+    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
@@ -41,17 +45,35 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChance = (event) => {
+    console.log('filter:', event.target.value.toUpperCase())
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = (filter ==='')
+    ? persons
+    : persons.filter(person => person.name.toLocaleUpperCase().includes(filter.toUpperCase()))
+
   return(
     <>
       <h2>Phonebook</h2>
-      <form>
+      <div>
+        filter shown with <input
+        id='filter'
+        value={filter}
+        onChange={handleFilterChance}/>
+      </div>
+      <form id='PhonebookEntries'>
         <div>
+          <h3>add a new</h3>
           name: <input
+            id='name'
             value={newName}
             onChange={handleNameChange}/>
         </div>
         <div>
           number: <input
+            id='number'
             value={newNumber}
             onChange={handeNumberChange} />
         </div>
@@ -60,7 +82,7 @@ const App = () => {
         </div>
       </form>
       <Persons
-        persons={persons}
+        persons={personsToShow}
       />
     </>
   )
