325f36828f01475547a8d24d348f0f6329cb5334
julio-murillo <35673318+julio-murillo@users.noreply.github.com>
2025-07-28 03:37:11 +0000

    Exercise 3.8: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,7 +6,11 @@ const maxId = 1000000
 
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+//app.use(morgan('tiny',))
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
     { 
