47549cd3a1d3b715731ffe639a24e8b06e321118
Julio Murillo <julio.murillo@gmail.com>
2025-08-10 21:58:34 -0600

    Excercise 3.13 - Phonebook database, step 1

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,15 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const Person = require('./models/person')
 const app = express()
 const minId = 1000
 const maxId = 1000000
 
-const allowedOrigin = 'http://localhost:5173/'
-
-app.use(cors())
-
+app.use(express.static('dist'))
 app.use(express.json())
 
 //app.use(morgan('tiny',))
@@ -17,54 +14,35 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 //retrieves all persons from the DB
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
+app.get('/api/people', (request, response) => {
+  Person.find({}).then(people => {
+    response.json(people)
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
+//retrieves the person identified by id
+app.get('/api/people/:id', (request, response) => {
+  console.log('Pending implementation using mongo DB')
+  {/*const id = request.params.id
   const person = persons.find(person => person.id === id)
 
   if(person) {
     response.json(person)
   } else {
     response.status(404).end()
-  }
+  }*/}
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
+//deletes the person identified by 1d
+app.delete('/api/people/:id', (request, response) => {
+  console.log('Pending implementation using mongo DB')
+  {/*const id = request.params.id
   console.log(`Persons before deletion: ${JSON.stringify(persons)}`)
   persons = persons.filter(person => person.id !== id)
   console.log(`Persons after deletion: ${JSON.stringify(persons)}`)
 
-  response.status(204).end()
+  response.status(204).end()*/}
 })
 
 const genId = () => Math.floor(Math.random() * (maxId - minId + 1) + minId)
@@ -99,9 +77,17 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-app.get('/info/', (request, response) => {
-    const people = persons.length
-
+app.get('info/', (request, response) => {
+  Person.countDocuments({})
+      .then((count) => {
+        console.log(`There's ${count} people in the collection`)
+        peopleCount = count
+        console.log(`Value assigned to var peopleCount is: ${peopleCount}`)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    
     const options = {
         weekday: 'short',
         year: 'numeric',
@@ -117,20 +103,25 @@ app.get('/info/', (request, response) => {
     const timeZoneName = Intl.DateTimeFormat([], {timeZoneName: 'long'})
       .formatToParts().find(part => part.type === 'timeZoneName').value
 
-    response.send(`
-        <h3>Phonebook has info for ${people} people</h3>
-        <h3>${formatter.format(Date.now())} (${timeZoneName})</h3>
+    Person.countDocuments({})
+      .then((count) => {
+        response.send(`
+          <h3>Phonebook has info for ${peopleCount} people</h3>
+          <h3>${formatter.format(Date.now())} (${timeZoneName})</h3>
         `)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
 })
 
-app.get('/', (request, response) => {
+{/*app.get('/', (request, response) => {
     response.send('<h1>This is the root.. nothing to see here!!!</h1>')
-})
+})*/}
 
 const PORT=3001
 app.listen(PORT, () => {
-
-    const serverTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-    console.log(`Server running on port ${PORT}`)
-    console.log(`The server's timezone is "${serverTimeZone}`)
+  const serverTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  console.log(`Server running on port ${PORT}`)
+  console.log(`The server's timezone is "${serverTimeZone}`)
 })
