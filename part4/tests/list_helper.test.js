72e416f65856a9c1000d6c10c76a9d6917958ce4
Julio Murillo <julio.murillo@gmail.com>
2025-08-24 11:13:24 -0600

    Exercise 4.5 - Helper Functions and Unit Test, step 3

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -70,7 +70,7 @@ describe('total likes', () => {
       title: 'First class tests',
       author: 'Robert C. Martin',
       url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-      likes: 10
+      likes: 0
     },
     {
       title: 'TDD harms architecture',
@@ -98,6 +98,77 @@ describe('total likes', () => {
 
   test('when list has several blogs, equals the sum of the likes of all blogs', () => {
     const result = listHelper.totalLikes(listWithSeveralBlogs)
-    assert.strictEqual(result, 87)
+    assert.strictEqual(result, 77)
+  })
+})
+
+describe('favorite blog', () => {
+  const listWithSeveralBlogs = [
+    {
+      title: 'Test blog',
+      author: 'Julio Murillo',
+      url: 'https://myblog.com',
+      likes: 5
+    },
+    {
+      title: 'Test blog 2',
+      author: 'Kathy Canessa',
+      url: 'https://herblog.com',
+      likes: 18
+    },
+    {
+      title: 'Test blog X',
+      author: 'Matias Murillo Canessa',
+      url: 'https://hisblog.com/Matias',
+      likes: 13
+    },
+    {
+      title: 'Test blog Y',
+      author: 'Camila Murillo Canessa',
+      url: 'https://theblog.com/Matias',
+      likes: 15
+    },
+    {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7
+    },
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5
+    },
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12
+    },
+    {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 18
+    },
+    {
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0
+    },
+    {
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2
+    }
+  ]
+
+  test('when list has several blogs, and more that one blog share the max value, return the first one', () => {
+    const result = listHelper.favoriteBlog(listWithSeveralBlogs)
+    //console.log ('This is the favorite blog', result)
+    assert.deepStrictEqual(result, { title: 'Test blog 2', author: 'Kathy Canessa', url: 'https://herblog.com', likes: 18 })
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const maxLikes =  Math.max(...blogs.map(blog => blog.likes))
+
+  return blogs.find(blog => blog.likes===maxLikes)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
